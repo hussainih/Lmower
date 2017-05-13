ed8417d68b79e5d707fc13b6bbe0cab67ff35d4e
Hussaini <hussaini.mohd86@gmail.com>
2017-05-13 16:50:15 +0200

    Rest test

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-$host = "lmower.ck1dzexlod8f.us-west-2.rds.amazonaws.com:3306";
+$host = "lmower.ck1dzexlod8f.us-west-2.rds.amazonaws.com";
 $user = "lmower";
 $pass = "lmowerpassword";
 $db_name = "lmower";
-$mysqli = new mysqli($host, $user, $pass, $db_name);
+$mysqli = new mysqli($host, $user, $pass, $db_name,3306);
 
