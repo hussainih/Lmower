533d08a39c1dc5755fa6bb8ab46a7ec50dadcab0
Hussaini <hussaini.mohd86@gmail.com>
2017-05-13 10:51:55 +0200

    Rest test

--- adnan.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<h1> this is adnan's php </h1>
-<?php
-echo "today is " . date("Y/m/d") . "<br>";
-
- ?>
-</body>
-</html>
--- kriti.php
+++
@@ -1,5 +0,0 @@
-<p>hi this is Kriti</p>
-<?php
-echo "today is " . date("Y/m/d") . "<br>";
-
- ?>
