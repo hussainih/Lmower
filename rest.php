78d5ff0e809540d2102dbbd42b2c8a3a7fd346c4
Hussaini <hussaini.mohd86@gmail.com>
2017-05-13 12:12:20 +0200

    Rest test

--- rest.php
+++ rest.php
@@ -3,5 +3,24 @@
 
 require 'vendor/autoload.php';
 $app = new Slim\App();
+$app->get('/books', function() {
+
+ //require_once('db.php');
+
+ //$query = "select * from library order by book_id";
+
+ //$result = $connection->query($query);
+
+ // var_dump($result);
+
+ //while ($row = $result->fetch_assoc()){
+
+//$data[] = $row;
+
+ //}
+
+ echo json_encode("hello there");
+
+});
 ?>
 
